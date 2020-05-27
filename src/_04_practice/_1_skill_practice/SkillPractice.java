a19effa5c1a34248ee5afe4360bce7939426d16c
Pallav <Pallav@DESKTOP-DVPQU53>
2020-05-27 16:33:44 -0700

    FINISHED THESE 2!

--- src/_04_practice/_1_skill_practice/SkillPractice.java
+++ src/_04_practice/_1_skill_practice/SkillPractice.java
@@ -60,51 +60,64 @@ for (int i = 1; i < 31; i++) {
 	 void skill3() {// Get a random number that is less than 20 and print it to the console 
 int y = 0;
 Random num = new Random();
-y = num.nextInt();
-System.out.println(num);
+y = num.nextInt(20);
+System.out.println(y);
 	// Get another random number that is less than 10 and print it to the console 
-
+int t = 0;
+Random number = new Random();
+t = num.nextInt(10);
+System.out.println(t);
 
 
 	// Using a pop-up, tell the user the difference between the numbers// Hint: use subtraction 
-
-
+ 
+JOptionPane.showMessageDialog(null, "The difference between the random numbers is "  + (y-t) + ".");
 
 		}
 
 	 void skill4() {// In a pop-up, ask the user for the city they live in 
 
-
+String city = JOptionPane.showInputDialog("What city do you live in?");
 
 	// If they answered "San Diego", tell them they live in America's Finest City 
-
+if (city.equalsIgnoreCase("San Diego")) {
+	JOptionPane.showMessageDialog(null, "You live in America's Finest City!");
+}
 
 
 	// Otherwise, tell them to move to San Diego 
-
+else {
+	JOptionPane.showMessageDialog(null, "You should move to San Diego!");
+}
 
 
 	// Create a variable - cars - and initialize it to the number of cars your family has.// If there are 0 cars, use a pop-up to display, "I bet you use public transportation." 
+String cars = JOptionPane.showInputDialog("How many cars do you have?");
 
-
-
+int s = Integer.parseInt(cars);
 	// If there is 1 car, use a pop-up to display the make/model of the car 
-
-
+if(s == 0) {
+	JOptionPane.showMessageDialog(null, "I bet you use public transportation!");
+}
+if (s == 1) {
+	JOptionPane.showMessageDialog(null, "You have a Nissan Rogue");
+}
 
 	// If there is more than 1 car, use a pop-up to display how many wheels the// cars have between them. 
 
-
+if (s > 1) {
+	JOptionPane.showMessageDialog(null, "You have " + (s*4) + " wheels!");
+}
 
 	 }
 
 	 void skill5() {// In a pop-up, ask the user for the name of their school 
 
-
+String school = JOptionPane.showInputDialog("What is the name of your school?");
 
 	// In another pop-up, tell the user, that their school is a fantastic school.// You must include the name of the school in the message. 
 
-
+JOptionPane.showMessageDialog(null, school + " is a fantastic school!");
 
 	 }
 	 }
--- src/_04_practice/_2_code_4_life/Code4Life.java
+++ src/_04_practice/_2_code_4_life/Code4Life.java
@@ -6,20 +6,32 @@ import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.swing.JApplet;
+import javax.swing.JOptionPane;
 
 public class Code4Life {
+public static void main(String[] args) {
+	
 
 	// 1. Ask the user how many hours they spent coding this week.
-
+	String CodingHours = JOptionPane.showInputDialog(null, "How many hours have you spent coding this week?");
+int CH = Integer.parseInt(CodingHours);
 	// 2. If it is less than or equal to 2, tell them to stop watching YouTube and
 	// write code instead.
-
+if (CH <= 2) {
+	JOptionPane.showMessageDialog(null, "Stop watching YouTube and write code instead!");
+}
 	// 3. If it is greater than or equal to 3 AND less than or equal to 5, tell them
 	// they're a Code Ninja
-
+if (3<=CH && CH<=5) {
+JOptionPane.showMessageDialog(null, "You are a Code Ninja!");	
+}
 	// 4. If it is more than 5, call the method below to play the Batman theme song.
 	// NOTE: You may have to get batman.wav from league-sounds on github
-
+if (CH > 5) {
+	JOptionPane.showMessageDialog(null, "One word for you, WOW!");
+playBatmanTheme();
+}
+}
 	private static void playBatmanTheme() {
 		try {
 			AudioClip sound = JApplet.newAudioClip(Code4Life.class.getResource("batman.wav"));
